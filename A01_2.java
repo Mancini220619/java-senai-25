a0ff20bd70c6b91e02880e2303f13baa8d2e9018
Mancini220619 <61313041+Mancini220619@users.noreply.github.com>
2025-04-26 16:51:26 -0300

    Create A01_2.java

---
+++ A01_2.java
@@ -0,0 +1,12 @@
+package com.mycompany.a01;
+
+    import java.util.Scanner;
+        public class A01_2 {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner (System.in);
+
+                System.out.println("Escreva alguma coisa aqui :");
+                String frase = scanner.nextLine();
+                System.out.println("Voce digitou: " + frase);
+    }
+}
